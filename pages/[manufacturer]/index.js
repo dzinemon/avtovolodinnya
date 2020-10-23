8a961c74190c2a6c00329799497afd7d14f73575
Andrii S <andriisharapatov@gmail.com>
2020-10-24 02:26:37 +0300

    naming

--- pages/[manufacturer]/index.js
+++ pages/[manufacturer]/index.js
@@ -1,8 +1,11 @@
 import { useEffect } from "react"
 
-import Breadcrumbs from '../../components/Breadcrumbs'
+import Breadcrumbs from '../../components/breadcrumbs'
 
-function Manufacturer() {
+import fs from 'fs'
+import path from 'path'
+
+function Manufacturer({availableModels}) {
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -17,9 +20,21 @@ function Manufacturer() {
       <div>
         Manufacturer page
       </div>
+
+      {availableManufacturers.map((i, idx) => (
+            <div key={idx} className="w-64 p-4">
+              <Link href={`/${i.filename.toLowerCase()}`}>
+                <a className="block hover:text-blue-600 transition transition-transform hover:scale-105 transform duration-500">
+                <div className="font-bold text-base md:text-xl">{i.filename.toUpperCase()}</div>
+                </a>
+              </Link>
+            </div>
+          ))}
       
     </div>
   )
 }
+
+export async function getStaticProps()
 
 export default Manufacturer
