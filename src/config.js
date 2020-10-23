9ac5e7b15adc5cfd019c25338c2d9df88c7425e5
Andrii S <andriisharapatov@gmail.com>
2020-10-23 23:25:14 +0300

    add src config file

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Вартість нових авто та повний перелік витрат при володінні',
+  author: {
+    name: 'Andrii Sharapatov',
+    summary: 'I love coffee, cars, coding',
+  },
+  description: 'Вартість нових авто та повний перелік витрати на володіння будь-яким авто',
+  social: {
+    twitter: 'andriisha',
+  },
+}
