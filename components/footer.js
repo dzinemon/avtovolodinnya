9370b16d3c699673081280cfbf1ebb65c55f259c
Andrii S <andriisharapatov@gmail.com>
2020-10-23 23:42:53 +0300

    add footer

---
+++ components/footer.js
@@ -0,0 +1,21 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="bg-gray-100">
+        <div className="xl:container mx-auto">
+          <section className="p-4">
+            <p>
+              Отримай повідомлення на свій e-mail
+            </p>
+            <hr className="my-4 border-gray-200"/>
+            <p>
+              <span>Вартість володіння авто</span>
+            </p>
+          </section>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
