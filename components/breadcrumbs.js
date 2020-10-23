0d1160733c92255242c887ebecdb2e64f8592b93
Andrii S <andriisharapatov@gmail.com>
2020-10-23 23:42:45 +0300

    add breadcrumbs

---
+++ components/breadcrumbs.js
@@ -0,0 +1,37 @@
+
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+function Breadcrumbs(props) {
+
+  const router = useRouter();
+
+  const {model, manufacturer} = router.query;
+
+  console.log(typeof model, model, typeof manufacturer, manufacturer);
+
+  return (
+    <div className="xl:container py-2 px-4 sm:p-4 mx-auto">
+      <div className="sm:text-xs text-sm">
+
+
+        <Link href="/" ><a className="text-gray-700 hover:text-gray-500">Головна</a></Link>
+        <span className="text-gray-400"> / </span> 
+        {!model && manufacturer && (
+          <>
+            <span className="text-gray-700 capitalize">{manufacturer}</span>           
+          </>
+        )}
+        {model && manufacturer && (
+          <>
+          <Link href={`/${manufacturer}`}><a className="text-gray-700 hover:text-gray-500 capitalize">{manufacturer}</a></Link> 
+          <span className="text-gray-400"> / </span>
+          <span className="text-gray-700 capitalize">{model.replaceAll('_', ' ')}</span>
+          </>
+          )} 
+      </div>
+    </div>
+  )
+}
+
+export default Breadcrumbs
