fa0c605c30b87e901ce9f246cb8dd95deec989b2
Andrii S <andriisharapatov@gmail.com>
2020-10-28 15:42:03 +0200

    adding structured data to breadcrumbs

--- components/breadcrumbs.js
+++ components/breadcrumbs.js
@@ -10,6 +10,37 @@ function Breadcrumbs(props) {
 
   return (
     <div className="xl:container py-2 px-4 sm:p-4 mx-auto">
+      <script type="application/ld+json">
+        {`{
+          "name": "Avtovolodinnya BreadcrumbList",
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          "itemListElement": [{
+            "@type": "ListItem",
+            "position": 1,
+            "name": "Головна",
+            "item": "https://avtovolodinnya.com/"
+          }
+          ${!model && manufacturer? `,{
+            "@type": "ListItem",
+            "position": 2,
+            "name": "${manufacturer.toUpperCase()}"
+          }` : ''}
+          ${model && manufacturer ? `
+          ,{
+            "@type": "ListItem",
+            "position": 2,
+            "name": "${manufacturer.toUpperCase()}",
+            "item": "https://avtovolodinnya.com/${manufacturer}"
+          }
+          ,{
+            "@type": "ListItem",
+            "position": 3,
+            "name": "${model.toUpperCase()}"
+          }` : ''}]
+        }`}
+        </script>
+
       <div className="sm:text-xs text-sm">
         <Link href="/" ><a className="text-gray-700 hover:text-gray-500">Головна</a></Link>
         <span className="text-gray-400"> / </span> 
