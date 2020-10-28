890566278ccd6310706225e89415366f1f97345d
Andrii S <andriisharapatov@gmail.com>
2020-10-29 00:34:02 +0200

    section one

--- components/home/sectionOne.js
+++ components/home/sectionOne.js
@@ -1,8 +1,8 @@
 function SectionOne() {
   return (
     <section className="max-w-4xl mx-auto py-10">
-      <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-        <div className="max-w-xs rounded overflow-hidden shadow-lg md:shadow-2xl my-2 transition transition-shadow duration-500 p-4">
+      <div className="grid gap-8 grid-cols-1 lg:grid-cols-3">
+        <div className="max-w-xs rounded overflow-hidden shadow-lg md:shadow-2xl my-2 mx-auto transition transition-shadow duration-500 p-4">
           <div className="text-4xl text-gray-900 font-bold text-center">
             5
           </div>
@@ -10,7 +10,7 @@ function SectionOne() {
             автовиробників з повним переліком моделей
           </div>
         </div>
-        <div className="max-w-xs rounded overflow-hidden shadow-lg md:shadow-2xl my-2 transition transition-shadow duration-500 p-4">
+        <div className="max-w-xs rounded overflow-hidden shadow-lg md:shadow-2xl my-2 mx-auto transition transition-shadow duration-500 p-4">
           <div className="text-4xl text-gray-900 font-bold text-center">
             69
           </div>
@@ -18,7 +18,7 @@ function SectionOne() {
             моделей з актуальними цінами
           </div>
         </div>
-        <div className="max-w-xs rounded overflow-hidden shadow-lg md:shadow-2xl my-2 transition transition-shadow duration-500 p-4">
+        <div className="max-w-xs rounded overflow-hidden shadow-lg md:shadow-2xl my-2 mx-auto transition transition-shadow duration-500 p-4">
           <div className="text-4xl text-gray-900 font-bold text-center">
             7+
           </div>
